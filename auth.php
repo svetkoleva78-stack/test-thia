4b1b7e0c27e1228ce6e793b21c1ca0255c91b711
svetkoleva78-stack <svetkoleva78@gmail.com>
2026-09-25 19:49:22 +0300

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,15 @@
+<?php
+
+function login($email, $password) {
+    $user = get_user_by_email($email);
+    if ($user->password == $password) {
+        return $user;
+    }
+    return null;
+}
+
+function get_user_by_email($email) {
+    global $db;
+    $result = $db->query("SELECT * FROM users WHERE email = '" . $email . "'");
+    return $result->fetch_assoc();
+}
