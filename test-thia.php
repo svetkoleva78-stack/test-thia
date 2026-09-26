91369701fae3c413f717a5d384b4757d501d0ac6
svetkoleva78-stack <svetkoleva78@gmail.com>
2026-09-26 15:28:46 +0300

    Add sum function to calculate sum of two numbers

--- test-thia.php
+++ test-thia.php
@@ -1,2 +1,13 @@
 <?php
 echo "Hello from Thia";
+
+/**
+ * Сума на две числа
+ *
+ * @param int|float $a Първо число
+ * @param int|float $b Второ число
+ * @return int|float Сбор на двете числа
+ */
+function sum($a, $b) {
+    return $a + $b;
+}
